d479de8524d4b6d6455cf5f6486c29ea99fe290e
joselerma97 <josemanuel@lermaserver.com>
2023-09-11 14:03:36 +0200

    files list

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if save_button:
 
 folder_path = './uploads'
 files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-st.subheader("Files:")
+st.subheader("Files in cloud:")
 for file in files:
     st.write(file)
 
